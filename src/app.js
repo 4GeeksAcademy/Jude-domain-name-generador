2f8242ecc849d5a99a43678f7b7bd63135c0fff6
cinjude <110071132+cinjude@users.noreply.github.com>
2025-10-22 04:26:16 +0000

    [I created a function in app.js so that I could get the sum of the arrays]

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+
+function getSumAllArray() {
+  let newArray = []
+  for (let pro of pronoun) {
+    for (let ad of adj) {
+      for (let n of noun) {
+        newArray.push(pro + ad + n + '.com' + "<br>")
+      }
+    }
+  }
+  return newArray
+}
+
+let domain = getSumAllArray()
+
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  document.querySelector("#exercise").innerHTML= domain
+
 };
